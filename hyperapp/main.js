b447418b14cff97238479cfa86eb9bd4235a13ea
amedog <americandog1993@gmail.com>
2018-01-21 23:41:29 +0900

    fix

--- hyperapp/main.js
+++ hyperapp/main.js
@@ -1,3 +1,7 @@
+import "../../node_modules/jquery/dist/jquery.js"
+import "../../node_modules/popper.js/dist/umd/popper.js"
+import "../../node_modules/bootstrap/dist/js/bootstrap.js"
+
 import { h, app } from "hyperapp"
 
 const state = {
