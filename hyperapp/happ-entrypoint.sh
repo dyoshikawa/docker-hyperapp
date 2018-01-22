d6b8b99cb8e5f8ce160465f95a7baf28c1853e26
dyoshikawa <yoshikawada@crunchtimer.jp>
2018-01-22 15:13:17 +0900

    fix happ-entrypoint.sh if分岐追加

--- hyperapp/happ-entrypoint.sh
+++ hyperapp/happ-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/sh
-sudo mv /var/www/happ-pre/* /var/www/happ/
-sudo mv /var/www/happ-pre/.??* /var/www/happ/
-sudo rmdir /var/www/happ-pre
+if [ -f /var/www/happ/package.json ]; then
+    sudo npm install
+    sudo npm run build
+else
+    sudo mv /var/www/happ-pre/* /var/www/happ/
+    sudo mv /var/www/happ-pre/.??* /var/www/happ/
+    sudo rmdir /var/www/happ-pre
+fi
 sudo chown -R 1000:1000 /var/www/happ
 sudo nginx -g 'daemon off;'
