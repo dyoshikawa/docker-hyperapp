5a49b5c5793116475927fccd60e2283aa03eceb1
dyoshikawa <yoshikawada@crunchtimer.jp>
2018-01-22 16:01:03 +0900

    Fix main.js -> main.jsx

--- hyperapp/webpack.config.js
+++ hyperapp/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/main.js',
+  entry: './src/js/main.jsx',
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, './dist')
